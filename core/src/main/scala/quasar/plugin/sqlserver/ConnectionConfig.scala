58c2c637ab1a3fb23fa608b5ae4a4316a0774e2e
Alissa Pajer <alissa.pajer@gmail.com>
2020-10-15 09:28:04 -0400

    WIP

--- core/src/main/scala/quasar/plugin/sqlserver/ConnectionConfig.scala
+++ core/src/main/scala/quasar/plugin/sqlserver/ConnectionConfig.scala
@@ -16,11 +16,133 @@
 
 package quasar.plugin.sqlserver
 
-import argonaut._
+import scala._, Predef._
 
-trait ConnectionConfig
+import argonaut._, Argonaut._, ArgonautCats._
+
+import cats._
+import cats.data.Validated
+import cats.implicits._
+
+import monocle.{Lens, Traversal}
+
+import quasar.plugin.jdbc.Redacted
+
+import shims.monoidToScalaz
+
+final case class ConnectionConfig(
+    baseUrl: String,
+    parameters: List[DriverParameter]) {
+
+  import ConnectionConfig._
+
+  def isSensitive: Boolean =
+    Optics.parameterNames.exist(SensitiveParameters)(this)
+
+  def jdbcUrl: String =
+    if (parameters.isEmpty)
+      baseUrl
+    else
+      s"$baseUrl;${parameters.map(_.forUrl).intercalate(";")}"
+
+  def mergeSensitive(other: ConnectionConfig): ConnectionConfig = {
+    val namesToMerge =
+      SensitiveParameters.filterNot(name => parameters.exists(_.name == name))
+
+    val paramsToMerge =
+      Optics.driverParameters
+        .foldMap(List(_).filter(p => namesToMerge(p.name)))(other)
+
+    Optics.parameters.modify(paramsToMerge ::: _)(this)
+  }
+
+  def sanitized: ConnectionConfig =
+    Optics.driverParameters
+      .modify(p =>
+        if (SensitiveParameters(p.name))
+          DriverParameter.Optics.value.set(Redacted)(p)
+        else
+          p
+      )(this)
+
+  def validated: Validated[String, ConnectionConfig] = {
+    val denied =
+      Optics.parameterNames
+        .getAll(this)
+        .filter(DeniedParameters)
+
+    Validated.cond(
+      denied.isEmpty,
+      this,
+      denied.mkString("Unsupported parameters: ", ", ", ""))
+  }
+}
 
 object ConnectionConfig {
+  val SensitiveParameters: Set[String] =
+    Set("password")
+
+  val DeniedParameters: Set[String] =
+    Set()
+
+  object Optics {
+    import shims.traverseToScalaz
+
+    val baseUrl: Lens[ConnectionConfig, String] =
+      Lens[ConnectionConfig, String](_.baseUrl)(u => _.copy(baseUrl = u))
+
+    val parameters: Lens[ConnectionConfig, List[DriverParameter]] =
+      Lens[ConnectionConfig, List[DriverParameter]](_.parameters)(ps => _.copy(parameters = ps))
+
+    val driverParameters: Traversal[ConnectionConfig, DriverParameter] =
+      parameters.composeTraversal(Traversal.fromTraverse[List, DriverParameter])
+
+    val parameterNames: Traversal[ConnectionConfig, String] =
+      driverParameters.composeLens(DriverParameter.Optics.name)
+  }
+
   implicit val connectionConfigCodecJson: CodecJson[ConnectionConfig] =
-    CodecJson(_ => Json.jEmptyObject, c => DecodeResult.fail("fixme", c.history))
+    CodecJson(
+      cc =>
+        ("jdbcUrl" := cc.jdbcUrl) ->:
+        jEmptyObject,
+
+      cursor => for {
+        urlString <- (cursor --\ "jdbcUrl").as[String]
+
+        queryStart = urlString.indexOf(';')
+
+        (baseUrl, query) =
+          if (queryStart < 0)
+            (urlString, "")
+          else
+            (
+              urlString.substring(0, queryStart),
+              urlString.substring(queryStart + 1, urlString.length)
+            )
+
+        separated =
+          if (query.isEmpty)
+            Nil
+          else
+            query.split(';').toList
+
+        params <- separated traverse {
+          case DriverParameter.NameValue(name, value) =>
+            DecodeResult.ok(DriverParameter(name, value))
+
+          case _ =>
+            DecodeResult.fail[DriverParameter]("Malformed driver parameter", cursor.history)
+        }
+      } yield ConnectionConfig(baseUrl, params))
+
+  implicit val connectionConfigEq: Eq[ConnectionConfig] =
+    Eq.by(cc => (
+      cc.baseUrl,
+      cc.parameters))
+
+  implicit val connectionConfigShow: Show[ConnectionConfig] =
+    Show show { cc =>
+      s"ConnectionConfig(${cc.jdbcUrl})"
+    }
 }
