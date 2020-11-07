ac374b449bd0c5162a7bfcb66dbeae56d9d054db
Alissa Pajer <alissa.pajer@gmail.com>
2020-11-07 14:35:47 -0500

    Implement SQL Server Destination

---
+++ destination/src/main/scala/quasar/plugin/sqlserver/destination/SQLServerDestinationModule.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2020 Precog Data
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.plugin.sqlserver.destination
+
+import quasar.plugin.sqlserver.{ConnectionConfig, DriverParameter}
+
+import scala._, Predef._
+import scala.concurrent.duration._
+
+import java.net.URI
+
+import argonaut._, Argonaut._
+
+import cats.data.NonEmptyList
+import cats.effect._
+import cats.implicits._
+
+import doobie.Transactor
+
+import org.slf4s.Logger
+
+import quasar.api.destination.DestinationType
+import quasar.connector.MonadResourceErr
+import quasar.connector.destination.{Destination, PushmiPullyu}
+import quasar.plugin.jdbc._
+import quasar.plugin.jdbc.destination._
+
+object SQLServerDestinationModule extends JdbcDestinationModule[DestinationConfig] {
+
+  val DefaultConnectionMaxConcurrency: Int = 16
+  val DefaultConnectionMaxLifetime: FiniteDuration = 5.minutes
+
+  val destinationType = DestinationType("sql-server", 1L)
+
+  def sanitizeDestinationConfig(config: Json): Json =
+    config.as[DestinationConfig].toOption.fold(jEmptyObject)(_.sanitized.asJson)
+
+  def transactorConfig(config: DestinationConfig)
+      : Either[NonEmptyList[String], TransactorConfig] =
+    for {
+      cc <- config.connectionConfig.validated.toEither.leftMap(NonEmptyList.one(_))
+
+      maxConcurrency = cc.maxConcurrency getOrElse DefaultConnectionMaxConcurrency
+      maxLifetime = cc.maxLifetime getOrElse DefaultConnectionMaxLifetime
+
+      connectionString =
+        ConnectionConfig.Optics.parameters
+          .modify(
+            DriverParameter.SendTemporalDataTypesAsStringForBulkCopy ::
+              DriverParameter.SendTimeAsDatetime ::
+              DriverParameter.LoginTimeout ::
+              _)(cc)
+          .jdbcUrl
+
+      jdbcUrl <-
+        Either.catchNonFatal(URI.create(connectionString)).leftMap(_ => NonEmptyList.one(
+          "Malformed JDBC connection string, ensure any restricted characters are properly escaped"))
+
+      txConfig =
+        TransactorConfig
+          .withDefaultTimeouts(
+            JdbcDriverConfig.JdbcDriverManagerConfig(
+              jdbcUrl,
+              Some("com.microsoft.sqlserver.jdbc.SQLServerDriver")),
+            connectionMaxConcurrency = maxConcurrency,
+            connectionReadOnly = false)
+          .copy(connectionMaxLifetime = maxLifetime)
+    } yield txConfig
+
+  def jdbcDestination[F[_]: ConcurrentEffect: ContextShift: MonadResourceErr: Timer](
+      config: DestinationConfig,
+      transactor: Transactor[F],
+      pushPull: PushmiPullyu[F],
+      log: Logger)
+      : Resource[F, Either[InitError, Destination[F]]] = {
+    val schema = config.schema.getOrElse("dbo")
+
+    // TODO test different schema
+    (new SQLServerDestination[F](config.writeMode, schema, transactor, log): Destination[F])
+      .asRight[InitError]
+      .pure[Resource[F, ?]]
+  }
+}
---
+++ destination/src/main/scala/quasar/plugin/sqlserver/destination/package.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2020 Precog Data
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.plugin.sqlserver
+
+import scala._, Predef._
+
+import quasar.connector.render.RenderConfig
+
+import cats.data.NonEmptyList
+import cats.implicits._
+
+import doobie._
+import doobie.implicits._
+
+package object destination {
+  val NullSentinel = ""
+
+  val SQLServerCsvConfig: RenderConfig.Csv =
+    RenderConfig.Csv(
+      includeHeader = false,
+      nullSentinel = Some(NullSentinel),
+      includeBom = false,
+      booleanFormat = if (_) "1" else "0")
+
+  def ifExists(logHandler: LogHandler)(
+      unsafeTable: String,
+      unsafeSchema: Option[String]): Query0[Int] = {
+    val schemaFragment = unsafeSchema
+      .map(s => fr0"' AND TABLE_SCHEMA='" ++ Fragment.const0(s))
+      .getOrElse(fr0"")
+
+    (fr0"SELECT count(*) as exists_flag FROM [INFORMATION_SCHEMA].[TABLES] WHERE TABLE_NAME='" ++
+        Fragment.const0(unsafeTable) ++
+        schemaFragment ++
+        fr0"'")
+      .queryWithLogHandler[Int](logHandler)
+  }
+
+  def replaceTable(logHandler: LogHandler)(
+      objFragment: Fragment,
+      columns: NonEmptyList[(HI, SQLServerType)])
+      : ConnectionIO[Int] = {
+    val drop = (fr"DROP TABLE IF EXISTS" ++ objFragment)
+      .updateWithLogHandler(logHandler)
+      .run
+
+    drop >> createTable(logHandler)(objFragment, columns)
+  }
+
+  def truncateTable(logHandler: LogHandler)(
+      objFragment: Fragment,
+      unsafeTable: String,
+      unsafeSchema: Option[String],
+      columns: NonEmptyList[(HI, SQLServerType)])
+      : ConnectionIO[Int] =
+    ifExists(logHandler)(unsafeTable, unsafeSchema).option flatMap { result =>
+      if (result.exists(_ == 1))
+        (fr"TRUNCATE TABLE" ++ objFragment)
+          .updateWithLogHandler(logHandler)
+          .run
+      else
+        createTable(logHandler)(objFragment, columns)
+    }
+
+  def appendToTable(logHandler: LogHandler)(
+      objFragment: Fragment,
+      unsafeTable: String,
+      unsafeSchema: Option[String],
+      columns: NonEmptyList[(HI, SQLServerType)])
+      : ConnectionIO[Int] =
+    ifExists(logHandler)(unsafeTable, unsafeSchema).option flatMap { result =>
+      if (result.exists(_ == 1))
+        0.pure[ConnectionIO]
+      else
+        createTable(logHandler)(objFragment, columns)
+    }
+
+  def createTable(logHandler: LogHandler)(
+      objFragment: Fragment,
+      columns: NonEmptyList[(HI, SQLServerType)])
+      : ConnectionIO[Int] =
+    (fr"CREATE TABLE" ++ objFragment ++ fr0" " ++ createColumnSpecs(columns))
+      .updateWithLogHandler(logHandler)
+      .run
+
+  def createColumnSpecs(cols: NonEmptyList[(HI, SQLServerType)]): Fragment =
+    Fragments.parentheses(
+      cols
+        .map { case (n, t) => Fragment.const(n.forSql) ++ t.asSql }
+        .intercalate(fr","))
+
+}
