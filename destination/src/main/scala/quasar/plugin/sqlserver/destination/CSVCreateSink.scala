5a01df10932ee74c5934c285f180c89e74b2e0be
Alissa Pajer <alissa.pajer@gmail.com>
2020-11-07 22:32:53 -0500

    wip

--- destination/src/main/scala/quasar/plugin/sqlserver/destination/CSVCreateSink.scala
+++ destination/src/main/scala/quasar/plugin/sqlserver/destination/CSVCreateSink.scala
@@ -170,7 +170,7 @@ private[destination] object CsvCreateSink {
     bytes => {
       Stream.resource(xa.connect(xa.kernel)) flatMap { connection =>
         val unwrapped = connection.unwrap(classOf[SQLServerConnection])
-        (bytes ++ Stream.emit(','.toByte) ++ Stream.emit('\r'.toByte) ++ Stream.emit('\n'.toByte)).onFinalize(ConcurrentEffect[F].delay(logger.debug(s"finished byte stream"))).observe(_.map(b => println(s"bytes: $b"))).through(fs2.io.toInputStream[F]).evalMap(doLoad(_, unwrapped).transact(xa))
+        (Stream.emit('a'.toByte) ++ bytes ++ Stream.emit(','.toByte) ++ Stream.emit('\r'.toByte) ++ Stream.emit('\n'.toByte)).onFinalize(ConcurrentEffect[F].delay(logger.debug(s"finished byte stream"))).observe(_.map(b => println(s"bytes: $b"))).through(fs2.io.toInputStream[F]).evalMap(doLoad(_, unwrapped).transact(xa))
       }
     }
   }
